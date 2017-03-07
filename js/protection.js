69a4680a1f80814b5493239e2914001d72f13870
myql <276466206@qq.com>
2017-03-07 22:33:12 +0800

    some

--- js/protection.js
+++
@@ -1,9 +0,0 @@
-var $detail=$("#container .protection-details li");
-console.log($detail);
-$detail.on('click',function(){
-	console.log(1);
-	$detail.css({
-		'border-bottom': '2px solid #ee0000',
-		'color': '#ee0000'
-	})
-})
