8662b2879cb6baac9725b506490b0f4f4e0ae426
276466206@qq.com <wuqiong@DuoLunTechnology.com>
2017-03-10 17:01:26 +0800

    some

--- js/protection.js
+++ js/protection.js
@@ -1,11 +1,14 @@
 var $protectionTitle=$(".protection-slogan"),
-titleTop=$protectionTitle.offset().top;
+titleTop=$protectionTitle.offset().top,
+$protectionLogan=$(".protection-logan");
 $(window).on('scroll',function(){
 	var windowTop=$(window).scrollTop();
 	if(windowTop>titleTop){
 		$protectionTitle.addClass('protection-fix');
+		$protectionLogan.addClass('logan-change');
 	}else {
 		$protectionTitle.removeClass('protection-fix');
+		$protectionLogan.removeClass('logan-change');
 	}
 	
 })
