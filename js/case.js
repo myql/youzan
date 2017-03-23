8a9a09aa2845536784fa699786901eff50f0e6ef
276466206@qq.com <wuqiong@DuoLunTechnology.com>
2017-03-23 14:53:30 +0800

    some

--- js/case.js
+++ js/case.js
@@ -1,31 +1,78 @@
-var $protectionCarousel=$('.case-slogan .protection-carousel'),
+var $caseSlogan=$('.case-slogan'),
 $shopAhead=$('.case-slogan .shop-ahead'),
 $shopBack=$('.case-slogan .shop-back'),
-$caseDot=$('.case-slogan .case-dot li'),
-$caseList=$('.case-slogan .protection-carousel .case-list');
-var idx=0;
+$caseDot=$('.case-slogan .case-dot'),
+$caseList=$('.case-slogan .case-list'),
+$caseLi=$('.case-slogan .case-list li');
+	var windowWidth=$(window).width();
+	$caseLi.width(windowWidth);
+	var imageWidth=$caseLi.width();
+var caseLis=$caseList.children();
+var imageCount=$caseList.children().length;
+$caseList.append(caseLis.first().clone());
+$caseList.prepend(caseLis.last().clone());
+var imageRealCount=$caseList.children().length;
+$caseList.css({
+	left: -imageWidth,
+})
+var curIdx=0;
 $shopAhead.on('click',function(){
 	playPre();
 })
 $shopBack.on('click',function(){
 	playNext();
 })
-$caseDot.on('click',function(){
+$caseDot.find('li').on('click',function(){
 	var $cur=$(this);
-	var curIdx=$cur.index();
+	var idx=$cur.index();
+	console.log(idx)
 	if(curIdx<idx){
-		playPre();
+		playNext(idx-curIdx);
 	}else if(curIdx>idx) {
-		playNext();
+		playPre(curIdx-idx);
 	}
-	console.log(curIdx);
 })
 var ifAnimate=false;
 function playNext(idx){
-	if(!ifAnimate){
 		idx=idx||1;
-		$caseList.animate({left:-idx},function(){
-			
+	if(!ifAnimate){
+		isAnimate=true;
+	
+		$caseList.animate({left:'-='+idx*imageWidth},'slow',function(){
+			curIdx=(curIdx+idx)%imageCount;
+			if(curIdx===0){
+				$caseList.css({
+					left: -imageWidth
+				})
+			}
+			isAnimate=false;
+			setButtle();
 		})
 	}
 }
+function playPre(idx){
+		idx=idx||1;
+	if(!ifAnimate){
+		isAnimate=true;
+	
+		$caseList.animate({left:'+='+idx*imageWidth},function(){
+			curIdx=(imageCount+curIdx-idx)%imageCount;
+			if(curIdx===(imageCount-1)){
+				$caseList.css({
+					left: -imageWidth*imageCount
+				})
+			}
+			isAnimate=false;
+			setButtle();
+		})
+	}
+}
+function setButtle(){
+	$caseDot.find('li').eq(curIdx).addClass('dot-active').siblings().removeClass('dot-active');
+}
+function autoPlay(){
+	setInterval(function(){
+		playNext();
+	},5000)
+}
+// autoPlay();
